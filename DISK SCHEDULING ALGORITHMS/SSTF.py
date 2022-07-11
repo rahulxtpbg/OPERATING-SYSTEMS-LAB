fc2a8398e6a66a8510cea9ba497b63d56d01f888
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2022-07-11 22:38:32 +0530

    All Hail Megatron!

---
+++ DISK SCHEDULING ALGORITHMS/SSTF.py
@@ -0,0 +1,23 @@
+def Allocation(rSeq, head):
+    seek_count = 0
+    distance = 0
+    curr_track = 0
+    aSeq = []
+
+    for i in range(n):
+        curr_track = min(rSeq)
+        aSeq.append(curr_track)
+        rSeq.remove(curr_track)
+        distance = abs(curr_track-head)
+        seek_count = seek_count+distance
+        head = curr_track
+
+    print('The seek sequence is:')
+    for i in range(n):
+        print(aSeq[i])
+
+
+rSeq = [176, 79, 34, 60, 92, 11, 41, 114]
+n = len(rSeq)
+head = 50
+Allocation(rSeq, head)
